f1a78d342d552217ed56d38e21760ebf9bff4428
richmen <108880264+richmenaft@users.noreply.github.com>
2023-11-17 14:24:08 +0500

    Another rename
    Renaming VisualMath to visualmath

--- VisualMath/wsgi.py
+++ VisualMath/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for VisualMath project.
+WSGI config for visualmath project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VisualMath.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'visualmath.settings')
 
 application = get_wsgi_application()
