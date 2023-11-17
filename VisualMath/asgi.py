c24e92008a95a48149f506e75173f9c4dad7d1ee
richmen <108880264+richmenaft@users.noreply.github.com>
2023-11-17 14:19:15 +0500

    Rename
    Renaming easyMath to VisualMath

--- VisualMath/asgi.py
+++ VisualMath/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for easyMath project.
+ASGI config for VisualMath project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'easyMath.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VisualMath.settings')
 
 application = get_asgi_application()
--- VisualMath/wsgi.py
+++ VisualMath/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for easyMath project.
+WSGI config for VisualMath project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'easyMath.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VisualMath.settings')
 
 application = get_wsgi_application()
