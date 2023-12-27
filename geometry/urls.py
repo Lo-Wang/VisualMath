6f31a1b09c628967bca2f9a1ed7139fa6eddd500
richmen <108880264+richmenaft@users.noreply.github.com>
2023-12-27 23:22:27 +0500

    New geometry topics
    Added new geometry topics

--- geometry/urls.py
+++ geometry/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('7_class/1_chapter/1_lesson', views.geometry_7_1_1, name='geometry_7_1_1'),
     path('7_class/2_chapter/1_lesson', views.geometry_7_2_1, name='geometry_7_2_1'),
     path('7_class/2_chapter/2_lesson', views.geometry_7_2_2, name='geometry_7_2_2'),
+    path('7_class/2_chapter/3_lesson', views.geometry_7_2_3, name='geometry_7_2_3'),
+    path('7_class/2_chapter/4_lesson', views.geometry_7_2_4, name='geometry_7_2_4'),
+    path('7_class/2_chapter/5_lesson', views.geometry_7_2_5, name='geometry_7_2_5'),
+    path('7_class/2_chapter/6_lesson', views.geometry_7_2_6, name='geometry_7_2_6'),
 ]
